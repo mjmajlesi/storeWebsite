783f170481942b48418aa02f25bb4f4e75915195
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-02-25 01:31:31 +0330

    edit and Change Ui Footer and navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,19 @@
-import React, { useContext } from "react";
+import React from "react";
 import Container from "./container";
 import { Link } from "react-router-dom";
 import Button from "./buttuns";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useAppContext } from "./AppContext";
-import { ThemeContext } from "../pages/toggleTheme";
 export default function NavbarSite(){
-  const {Toggle , theme} = useContext(ThemeContext)
 
   const {getQTY , login , handleOutlogin} = useAppContext()
     return(
         <Container>
             <nav className=" grid grid-cols-12 mb-4 border-b-2 border-slate-800 p-2">
-              <span className=" text-3xl lg:col-span-4">Mjm</span>
+              <span className=" text-3xl lg:col-span-4 text-gray-300">Mjm</span>
                 <ul className="flex gap-5 justify-center items-center col-span-4">
                   <li><Link to={"/"} >Home</Link></li>
                   <li><Link to={"/store"} >Store</Link></li>
-                  <Button onClick={()=> Toggle()} >{theme}</Button>
                 </ul>
                 <div className="col-span-4 relative">
                   <Link to={'/cart'}>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,49 +1,45 @@
-import Container from './container'
-import kasbokar from '../images/kasbokar.webp';
-import rezaiat from '../images/rezi.webp';
-import telegram from '../images/footer/telegram-3-512.jpg'
-import email from '../images/footer/email-14-512.jpg'
-import github from '../images/footer/github-9-512.jpg'
-import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
+import Container from "../components/container";
+import { IconContext } from "react-icons";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-        <Container>
-            <div className='flex items-center justify-between pt-5 pb-4 border-t-4'>
-                <div className='flex justify-between items-center gap-10  pl-5'>
-                    <img width={80} src={kasbokar} alt="kasbokar" />
-                    <img width={100} src={rezaiat} alt="rezi" />
-                </div>
-                <div className='flex flex-col items-center justify-center gap-4 pt-11'>
-                    <div className='flex items-center justify-between gap-3'>
-                        <a href='https://t.me/Mj_majlesi' target='_blank' rel="noreferrer">
-                            <img width={40} src={telegram} alt="telegram" />
-                        </a>
-                        <a href='mohammadjavadmajlesi313@gmail.com' target='_blank' rel="noreferrer">
-                            <img width={40} src={email} alt="email" />
-                        </a>
-                        <a href='https://github.com/mjmajlesi' target='_blank' rel="noreferrer">
-                            <img width={40} src={github} alt="github" />
-                        </a>
-                    </div>
-                    <div>
-                        <span className='block text-center'> © 2024 Copyright: Mohammad Javad Majlesi </span>
-                    </div>
-                </div>
-
-                <div className='flex flex-col items-center justify-center gap-2 pr-5'>
-                    <h1 className='text-3xl'>Quick access</h1>
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/store'}>Store</Link>
-                    <Link to={'/card'} >CardShopping</Link>
-                </div>
+    <div className="mb-3">
+      <Container>
+        <hr />
+        <div className="flex items-center gap-10 lg:justify-between mt-5">
+          <span className="max-md:text-xs md:font-semibold">
+            © 2024 Copyright: Mohammad Javad Majlesi
+          </span>
+          <IconContext.Provider value={{ size: "30px" }}>
+            <div className="flex items-center gap-3">
+              <a
+                href="https://github.com/mjmajlesi"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://t.me/Mj_majlesi"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTelegramPlane />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/mohammad-javad-majlesi-0ab27331a/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
             </div>
-            <div>
-            </div>
-        </Container>
+          </IconContext.Provider>
+        </div>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
